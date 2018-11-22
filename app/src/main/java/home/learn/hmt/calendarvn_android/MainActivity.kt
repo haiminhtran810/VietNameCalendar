03c0c70610449dc8d6309c2b7a88953bfa26cb15
haiminhtran810 <haiminhtran810@gmail.com>
2018-11-22 16:05:37 +0700

    Innit Android

---
+++ app/src/main/java/home/learn/hmt/calendarvn_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package home.learn.hmt.calendarvn_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
