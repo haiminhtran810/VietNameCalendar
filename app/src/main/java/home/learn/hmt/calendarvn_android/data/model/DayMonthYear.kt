30271d112ed77c667cda26bd3a0152ec3896e45c
haiminhtran810 <haiminhtran810@gmail.com>
2018-11-29 16:52:10 +0700

    [Task] Add ViewPage

--- app/src/main/java/home/learn/hmt/calendarvn_android/data/model/DayMonthYear.kt
+++ app/src/main/java/home/learn/hmt/calendarvn_android/data/model/DayMonthYear.kt
@@ -1,10 +1,39 @@
 package home.learn.hmt.calendarvn_android.data.model
 
-data class DayMonthYear(
-    val day: Int,
-    val month: Int,
-    val year: Int,
-    val nm: Int,
-    val leap: Int,
-    val full: Int
-)
+class DayMonthYear {
+    var day: Int = 0
+    var month: Int = 0
+    var year: Int = 0
+    var nm: Int = 0
+    var leap: Int = 0
+    var full: Int = 0
+
+    constructor(day: Int, month: Int, year: Int, nm: Int, leap: Int) {
+        this.day = day
+        this.month = month
+        this.year = year
+        this.nm = nm
+        this.leap = leap
+    }
+
+    constructor(day: Int, month: Int, year: Int, leap: Int) {
+        this.day = day
+        this.month = month
+        this.year = year
+        this.leap = leap
+    }
+
+    constructor(day: Int, month: Int, year: Int) {
+        this.day = day
+        this.month = month
+        this.year = year
+    }
+
+    constructor(day: Int, month: Int) {
+        this.day = day
+        this.month = month
+        this.year = 0
+    }
+
+    constructor() {}
+}
